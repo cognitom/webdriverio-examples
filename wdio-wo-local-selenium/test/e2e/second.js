e98b89bfc7d0bdcebaa60afc762fb8f20d21ade0
Dustin Truong <dtruong@Dustins-MacBook-Pro.local>
2017-04-28 11:56:37 +1000

    updated test assertion

--- wdio-wo-local-selenium/test/e2e/second.js
+++ wdio-wo-local-selenium/test/e2e/second.js
@@ -4,6 +4,6 @@ const assert = require('assert')
 describe('Second Test Group', () => {
   it('gets the title of GitHub toppage', () => {
     const title = browser.url('https://github.com/').getTitle()
-    assert.equal(title, 'How people build software · GitHub')
+    assert.equal(title, 'The world\'s leading software development platform · GitHub')
   })
 })
