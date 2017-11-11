882a418696be7629e8d0097021dd0b46a8c7fc91
kan-ki <escapefr2@gmail.com>
2017-11-11 11:25:39 +0900

    Fix the title error of MDN site.

--- wdio-phantomjs-only/test/e2e/first.js
+++ wdio-phantomjs-only/test/e2e/first.js
@@ -4,6 +4,6 @@ const assert = require('assert')
 describe('First Test Group', () => {
   it('gets the title of MDN toppage', () => {
     const title = browser.url('https://developer.mozilla.org/en-US/').getTitle()
-    assert.equal(title, 'Mozilla Developer Network')
+    assert.equal(title, 'MDN Web Docs')
   })
 })
